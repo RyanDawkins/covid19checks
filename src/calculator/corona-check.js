d26dbf16c164862efa604636f90dcf2b0fd045de
Ryan Dawkins <ryandawk@amazon.com>
2020-03-19 23:13:22 -0700

    Adding corona check calculations

---
+++ src/calculator/corona-check.js
@@ -0,0 +1,76 @@
+const SINGLE = {
+    baseAmount: 1200,
+    maxIncomeForFullPayout: 75000,
+    minIncomeForPayout: 2500,
+    minimumPayout: 1200
+}
+
+const MARRIED = {
+    baseAmount: 2400,
+    maxIncomeForFullPayout: 150000,
+    minIncomeForPayout: 2500,
+    minimumPayout: 600
+}
+
+const DEDUCTION_PER_INCREMENT = 5
+const INCREMENT = 100
+
+export default class CoronaCheck {
+
+    /**
+     * @param {number} income 
+     * @param {boolean} isMarried 
+     */
+    constructor(income, isMarried) {
+        this._income = income
+        this._isMarried = isMarried
+    }
+
+    /**
+     * @returns {number} payout based on income and marital status
+     */
+    calculatePayout() {
+        const maritalValues = this._getMaritalValues()
+        return maritalValues.baseAmount - this._getDeductionOffset(maritalValues.maxIncomeForFullPayout)
+    }
+
+    _getMaritalValues() {
+        return this.isMarried() ? MARRIED : SINGLE
+    }
+
+    _getDeductionOffset(maxIncomeForFullPayout) {
+        if (this.getIncome() <= maxIncomeForFullPayout) return 0
+        return Math.round(((this.getIncome() - maxIncomeForFullPayout) / INCREMENT) * DEDUCTION_PER_INCREMENT)
+    }
+
+    /**
+     * @param {number} income
+     * @returns {CoronaCheck} new instance with a different income
+     */
+    changeIncome(income) {
+        return new CoronaCheck(income, this.isMarried())
+    }
+
+    /**
+     * @param {boolean} isMarried true if married, false if single
+     * @returns {CoronaCheck} new instance with a different marital status
+     */
+    changeMaritalStatus(isMarried) {
+        return new CoronaCheck(this.getIncome(), isMarried)
+    }
+
+    /**
+     * @returns {boolean} true if married, false if single
+     */
+    isMarried() {
+        return this._isMarried
+    }
+
+    /**
+     * @returns {number} income in USD
+     */
+    getIncome() {
+        return this._income
+    }
+
+}
---
+++ src/calculator/corona-check.test.js
@@ -0,0 +1,72 @@
+import CoronaCheck from './corona-check'
+
+const MARRIED = true
+const SINGLE = false
+
+describe('CoronaCheck', function () {
+
+    describe('calculate', function () {
+
+        it('should have base when single has below the max income', function () {
+            const check = new CoronaCheck(74999, SINGLE)
+            expect(check.calculatePayout()).toBe(1200)
+        })
+
+        it('should have base when single has the max income', function () {
+            const check = new CoronaCheck(75000, SINGLE)
+            expect(check.calculatePayout()).toBe(1200)
+        })
+
+        it('should deduct when married has above the max income', function() {
+            const check = new CoronaCheck(75200, SINGLE)
+            expect(check.calculatePayout()).toBe(1190)
+        })
+
+        it('should have base when married has below the max income', function () {
+            const check = new CoronaCheck(149999, MARRIED)
+            expect(check.calculatePayout()).toBe(2400)
+        })
+
+        it('should have base when married has the max income', function () {
+            const check = new CoronaCheck(150000, MARRIED)
+            expect(check.calculatePayout()).toBe(2400)
+        })
+
+        it('should deduct when married has above the max income', function() {
+            const check = new CoronaCheck(150200, MARRIED)
+            expect(check.calculatePayout()).toBe(2390)
+        })
+
+        it('should not mutate when changing marital status', function () {
+            const check = new CoronaCheck(150000, MARRIED)
+            check.changeMaritalStatus(SINGLE)
+
+            expect(check.isMarried()).toBe(true)
+        })
+
+        it('should not mutate when changing income', function () {
+            const check = new CoronaCheck(150000, MARRIED)
+            check.changeMaritalStatus(SINGLE)
+
+            expect(check.getIncome()).toBe(150000)
+        })
+
+        it('should change payout when changing marital status', function () {
+            let check = new CoronaCheck(75200, SINGLE)
+
+            check = check.changeMaritalStatus(MARRIED)
+
+            expect(check.calculatePayout()).toBe(2400)
+        })
+
+        it('should change payout when changing income', function () {
+            let check = new CoronaCheck(75200, SINGLE)
+
+            check = check.changeIncome(75000)
+
+            expect(check.calculatePayout()).toBe(1200)
+        })
+
+    })
+
+})
