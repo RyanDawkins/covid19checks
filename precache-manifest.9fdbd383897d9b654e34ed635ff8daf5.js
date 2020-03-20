51e9a9e9a10af1cfa6ec597e607a28e322efd954
Ryan Dawkins <ryandawk@amazon.com>
2020-03-19 23:16:08 -0700

    Updates

---
+++ precache-manifest.9fdbd383897d9b654e34ed635ff8daf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dba0576076d749b5bd1386ce0874308",
+    "url": "/covid-checks/index.html"
+  },
+  {
+    "revision": "cfab23901bef57b396aa",
+    "url": "/covid-checks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ccc0e0b5df94762e669b",
+    "url": "/covid-checks/static/js/2.8d46ca74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-checks/static/js/2.8d46ca74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfab23901bef57b396aa",
+    "url": "/covid-checks/static/js/main.43b86f2b.chunk.js"
+  },
+  {
+    "revision": "6e268c7e3cc769c42f88",
+    "url": "/covid-checks/static/js/runtime-main.bba94989.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-checks/static/media/logo.5d5d9eef.svg"
+  }
+]);
